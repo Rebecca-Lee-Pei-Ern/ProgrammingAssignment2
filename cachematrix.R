b9242f12bde81ad205ccb9ba2a7ed471a708d7be
Rebecca Lee Pei Ern <rlpe0720@gmail.com>
2020-10-24 00:26:10 +0800

    Added descriptions

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## makeCacheMatrix - to create a matrix and cache its inverse
+## cacheSolve - to return inverse of the matrix if already exist on cache returned by makeCacheMatrix
 
 ## Write a short comment describing this function
-
+## to store a returned matrix in the global environment
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -17,7 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## to check if the inverse matrix already exist and retrieve the stored matrix from previous function, then return the inverse matrix
+## if not, it will return the incerse matrix and store in makeCacheMatrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   inv <- x$getinverse()
